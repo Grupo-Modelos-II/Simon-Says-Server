86c828ce45196d65e78b3a55c27fbd72cd488fb2
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-15 08:24:00 -0500

    Use of functions of entity module

--- src/controller/player.ts
+++ src/controller/player.ts
@@ -1,11 +1,25 @@
-import { Router } from 'express';
-
+import { Router,Response,Request } from 'express';
 import { PlayerRequest } from '../dto/player';
+import { Player,createPlayer,getPlayers } from '../entity/player';
 
 const playerController: Router = Router();
 
-playerController.get('/', ({body}: {body: PlayerRequest}, res) => {
-    return res.send(body);
+playerController.get('/', (_: Request, response:Response) => {
+    getPlayers().then((res:Player[]) => {
+        response.status(200).json(res);
+    }).catch((error:any) => {
+        response.status(400).json(error);
+    });
+});
+
+playerController.post('/create',({body}: {body: PlayerRequest},response:Response) => {
+    let dataBody:any = {'id_user':Date.now(),...body}
+    createPlayer(dataBody).then((res:Player) => {
+        response.status(200).json(res);
+    }).catch((error:any) => {
+        console.log(error);
+        response.status(400).json(error);
+    });
 });
 
 export default playerController;
