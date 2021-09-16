908d2a11a2ce6757218567822f6f497912c00f1e
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-15 22:13:25 -0500

    update schema database

--- db/Schema.sql
+++ db/Schema.sql
@@ -6,3 +6,20 @@ create table Player(
 );
 
 alter table Player add constraint pk_user primary key(id_user);
+
+create table Game(
+    id_game varchar not null,
+    score bigint not null
+);
+
+alter table Game add constraint pk_game primary key(id_game);
+
+create table History(
+    id_user varchar not null,
+    id_game varchar not null,
+    hour_player date not null
+);
+
+alter table History add constraint pk_History primary key (id_user, id_game);
+alter table History add constraint fk_History_Player foreign key (id_user) references Player (id_user);
+alter table History add constraint fk_History_Game foreign key (id_game) references Game (id_game);
