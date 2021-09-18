c76628dfc4dbfc4453ed7a8240d9e9924d3df225
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-18 18:39:21 -0500

    Update Schema.sql

--- db/Schema.sql
+++ db/Schema.sql
@@ -4,7 +4,8 @@ create table Player(
     id_user varchar not null,
     name varchar not null,
     pass varchar not null,
-    max_score bigint not null default 0
+    max_score bigint not null default 0,
+    token_session varchar
 );
 
 alter table Player add constraint pk_user primary key(id_user);
