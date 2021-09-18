fe4d251288f98c278030d0926832399ed88788d6
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-18 18:42:25 -0500

    Rename module for securityParser

---
+++ src/util/securityParser.ts
@@ -0,0 +1,18 @@
+import { hash,compare } from 'bcrypt';
+import { sign,verify } from 'jsonwebtoken';
+
+export const encodePass = async (pass:string):Promise<string> => {
+    return await hash(pass,parseInt(process.env.SALTS as string));
+}
+
+export const verifyPassword = async (pass:string,hashPass:string):Promise<boolean> => {
+    return await compare(pass,hashPass);
+}
+
+export const authorizedUserProfile = async(dataToken:any):Promise<string> => {
+    return await sign(dataToken,process.env.TOKEN_SECRET as string);
+}
+
+export const decodePayload = async (token:string):Promise<any> => {
+    return await verify(token,process.env.TOKEN_SECRET as string);
+}
