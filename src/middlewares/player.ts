b9e54650ac9c1405da577d19c3d8a208f8c9090c
Sergio David Paez Suarez <spaezsuarez@gmail.com>
2021-09-18 18:41:46 -0500

    Initial middleware for player authorization

--- src/middlewares/player.ts
+++ src/middlewares/player.ts
@@ -1,10 +1,18 @@
 import { Response } from 'express';
 import { PlayerEntity } from '../entity/player';
 import { encode } from '../util/hashParser';
+import { decodePayload } from '../util/securityParser';
 
-export const playerEntityMiddleware = async (request:any,response:Response,next:any) => {
+export const playerCreationMiddleware = async (request:any,response:Response,next:any):Promise<void> => {
     let playerData:PlayerEntity = new PlayerEntity({id_user:`player_${Date.now()}`,...request.body});
     playerData.setPass(await encode(playerData.getPass()));
     request.playerData = playerData;
+    next();
+}
+
+export const playerAuthenticationMiddleware = async (request:any,response:Response,next:any):Promise<void> => {
+    const { user_token } = request.body;
+    let payloadToken:any = await decodePayload(user_token);
+    console.log(payloadToken);
     next();
 }
